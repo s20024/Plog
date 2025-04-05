0a38e9a062875d52ee554386ec9073426cfd1afc
Yara <s20024@std.it-college.ac.jp>
2025-04-05 19:35:19 +0900

    リンクをtarget=_blankに変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,23 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
+import rehypeExternalLinks from 'rehype-external-links';
 
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), react()],
+  integrations: [
+    mdx({
+      rehypePlugins: [
+        [
+          rehypeExternalLinks,
+          {
+            target: '_blank',
+            rel: 'noopener noreferrer',
+          },
+        ],
+      ],
+    }),
+    sitemap(),
+    react(),
+  ],
 });
